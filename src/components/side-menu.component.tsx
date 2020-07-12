df03814998171e82e7dcd94a7e4620fd02b656c6
Yurrian Pierre-Boyer <ryanx101@gmail.com>
2020-07-12 16:34:34 -0400

    Fixing Side Menu

--- src/components/side-menu.component.tsx
+++ src/components/side-menu.component.tsx
@@ -1,48 +1,57 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
-import HelpIcon from '@material-ui/icons/Help';
-import Toolbar from '@material-ui/core/Toolbar';
-import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
-import LiveHelpIcon from '@material-ui/icons/LiveHelp';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import React from "react";
+import { withRouter, RouteComponentProps } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MailIcon from "@material-ui/icons/Mail";
+import MenuIcon from "@material-ui/icons/Menu";
+import HelpIcon from "@material-ui/icons/Help";
+import Toolbar from "@material-ui/core/Toolbar";
+import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+import LiveHelpIcon from "@material-ui/icons/LiveHelp";
+import Typography from "@material-ui/core/Typography";
+import {
+  makeStyles,
+  useTheme,
+  Theme,
+  createStyles,
+} from "@material-ui/core/styles";
+import { useHistory } from "react-router";
+import zIndex from "@material-ui/core/styles/zIndex";
+import { Box } from "@material-ui/core";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: 'flex',
+      display: "flex",
     },
     drawer: {
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up("sm")]: {
         width: drawerWidth,
         flexShrink: 0,
       },
     },
     appBar: {
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up("sm")]: {
         width: `calc(100% - ${drawerWidth}px)`,
         marginLeft: drawerWidth,
       },
     },
     menuButton: {
       marginRight: theme.spacing(2),
-      [theme.breakpoints.up('sm')]: {
-        display: 'none',
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
       },
     },
     // necessary for content to be below app bar
@@ -54,7 +63,7 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
       padding: theme.spacing(3),
     },
-  }),
+  })
 );
 
 interface Props {
@@ -70,6 +79,7 @@ export default function SideMenuComponent(props: Props) {
   const classes = useStyles();
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const history = useHistory();
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -78,41 +88,59 @@ export default function SideMenuComponent(props: Props) {
   const drawer = (
     <div>
       <div className={classes.toolbar} />
-      
+
       <Typography variant="h6" noWrap>
-            Menu
-          </Typography>
-          {/* <Divider /> */}
+        Menu
+      </Typography>
+      {/* <Divider /> */}
       <List>
-        {['Feed', 'Post A Question'].map((text, index) => (
-          <ListItem button key={text}>
+        {["Feed"].map((text, index) => (
+          <ListItem
+            onClick={() => history.push("/feed")}
+            style={{ color: "#f26925" }}
+            button
+            key={text}
+          >
             <ListItemIcon>
-                {index % 2 === 0 ?
-                
-                <LiveHelpIcon style={{ color: 'orangered'  }} /> : 
-                <QuestionAnswerIcon style={{ color: 'orangered' }} />}</ListItemIcon>
+              <LiveHelpIcon
+                onClick={() => history.push("/feed")}
+                style={{ color: "#f26925" }}
+              />
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+        {["Post A Question"].map((text, index) => (
+          <ListItem
+            onClick={() => {
+              history.push("/question");
+            }}
+            style={{ color: "#f26925" }}
+            button
+            key={text}
+          >
+            <ListItemIcon>
+              <QuestionAnswerIcon
+                onClick={() => {
+                  history.push("/question");
+                }}
+                style={{ color: "#f26925" }}
+              />
+            </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
       </List>
-      {/* <Divider /> */}
-      {/* <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List> */}
     </div>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <div className={classes.root}>
+    <Box className={classes.root}>
       <CssBaseline />
-      {/* <AppBar position="fixed" className={classes.appBar}>
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
           <IconButton
             color="inherit"
@@ -127,14 +155,14 @@ export default function SideMenuComponent(props: Props) {
             Responsive drawer
           </Typography>
         </Toolbar>
-      </AppBar> */}
+      </AppBar>
       <nav className={classes.drawer} aria-label="mailbox folders">
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Hidden smUp implementation="css">
           <Drawer
             container={container}
             variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            anchor={theme.direction === "rtl" ? "right" : "left"}
             open={mobileOpen}
             onClose={handleDrawerToggle}
             classes={{
@@ -159,7 +187,6 @@ export default function SideMenuComponent(props: Props) {
           </Drawer>
         </Hidden>
       </nav>
-    </div>
+    </Box>
   );
 }
- 
