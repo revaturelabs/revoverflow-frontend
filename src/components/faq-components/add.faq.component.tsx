b4814d4f7ef9769e1f69d24bbe193bca4e4140fd
Colin Shaw <colin.shaw2@uoit.net>
2021-01-27 12:59:00 -0500

    added backdrop

--- src/components/faq-components/add.faq.component.tsx
+++ src/components/faq-components/add.faq.component.tsx
@@ -1,30 +1,103 @@
-import { Input } from "@material-ui/core";
-import React, { useState, FC } from "react";
+// import { Input } from "@material-ui/core";
+// import React, { useState, FC } from "react";
+// import { Answer } from "../../models/answer";
+// import { Question } from "../../models/question";
+
+// export interface AddFAQComponentProps{
+  
+// }
+
+
+// export const AddFAQComponent: React.FC<AddFAQComponentProps> = (props) => {
+//   const [question, setQuestion] = useState<Question>();
+//   const [answer, setAnswer] = useState<Answer>();
+
+//   const submitFAQ = (question:string, answer:string) =>{
+//     console.log("submitting FAQ")
+//     console.log("with question" + question)
+//     console.log("with answer" + answer)
+//   }
+
+//   return (
+//     <form id="addFAQForm" onSubmit={() =>submitFAQ}>
+//       <label>Question:</label>
+//       <input type="text" id="questionInput" />
+//       <label>Answer:</label>
+//       <input type="text" id="answerInput" />
+//       <button type="submit" id="submitFAQButton"/>
+//     </form>
+//   );
+// };
+
+import classes from "*.module.css";
+import { Backdrop, Card, Input } from "@material-ui/core";
+import axios from "axios";
+import React, { useState, FC, HtmlHTMLAttributes, SyntheticEvent, useEffect } from "react";
+import { StringDecoder } from "string_decoder";
 import { Answer } from "../../models/answer";
 import { Question } from "../../models/question";
 
-export interface AddFAQComponentProps{
-  
+//pass in null if there is no default question
+export interface AddFAQComponentProps {
+  defaultQuestion?:string
 }
 
-
 export const AddFAQComponent: React.FC<AddFAQComponentProps> = (props) => {
-  const [question, setQuestion] = useState<Question>();
-  const [answer, setAnswer] = useState<Answer>();
+  const [question, setQuestion] = useState<String>('');
+  const [answer, setAnswer] = useState<String>('');
 
-  const submitFAQ = (question:string, answer:string) =>{
-    console.log("submitting FAQ")
-    console.log("with question" + question)
-    console.log("with answer" + answer)
+  //get the truthiness of the defaultQuestion
+  let defaultQuestionProvided:boolean = (props.defaultQuestion)?true:false;
+
+  //if there is a default question set our question equal to it
+  useEffect(() => {
+    console.log("defaultQuestionProvided is " + defaultQuestionProvided)
+    setQuestion(props.defaultQuestion ?? '')
+  },[])
+
+  const handleQuestionChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    console.log('Changing question')
+    setQuestion(e.target.value)
+  }
+  const handleAnswerChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    console.log('Changing Answer')
+    setAnswer(e.target.value)
   }
 
+  const submitFAQ = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log("submitting FAQ");
+    console.log("with question " + question);
+    console.log("with answer " + answer);
+    //make an axios request to /faqs
+    //TODO refactor this
+    try {
+      axios
+        .post(
+          `http://localhost:8080/faq`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => {
+        });
+    } catch (e) {
+    }
+
+
+  };
+
   return (
-    <form id="addFAQForm" onSubmit={() =>submitFAQ}>
+    <Card>
+    <form id="addFAQForm" onSubmit={submitFAQ}>
       <label>Question:</label>
-      <input type="text" id="questionInput" />
+      <input type="text" id="questionInput" onChange={handleQuestionChange} placeholder="Enter your Question"/>
       <label>Answer:</label>
-      <input type="text" id="answerInput" />
-      <button type="submit" id="submitFAQButton"/>
+      <input type="text" id="answerInput" onChange={ handleAnswerChange} placeholder="Enter your Answer"/>
+      <button type="submit" id="submitFAQButton"> Submit </button>
     </form>
+    </Card>
   );
 };
--- src/components/pages/feed-components/feed-box.component.tsx
+++ src/components/pages/feed-components/feed-box.component.tsx
@@ -3,8 +3,8 @@
  * @author Keith Salzman 
  */
 
-import React from 'react';
-import { makeStyles, Box, Card } from '@material-ui/core';
+import React, { useState } from 'react';
+import { makeStyles, Box, Card, Backdrop } from '@material-ui/core';
 import { useHistory } from 'react-router';
 import { Question } from '../../../models/question';
 import * as answerRemote from '../../../remotes/answer.remote';
@@ -14,6 +14,7 @@ import { connect } from 'react-redux';
 import { clickQuestion } from '../../../actions/question.actions';
 import { convertFromRaw, EditorState, Editor } from 'draft-js';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import { AddFAQComponent } from '../../faq-components/add.faq.component';
 
 
 
@@ -29,6 +30,9 @@ const useStyles = makeStyles({
     },
     divInternal: {
         paddingTop: 20
+    },
+    backdrop: {
+
     }
 });
 
@@ -42,11 +46,18 @@ export interface FeedBoxComponentProps {
 export const FeedBoxComponent: React.FC<FeedBoxComponentProps> = (props) => {
     const classes = useStyles();
     const history = useHistory();
+    const [open, setOpen] = useState<boolean>(false);
+
+    const handleClose = () => {
+        console.log("closing")
+        setOpen(false)
+    }
 
     /**
      * retrieves answers, persists question in the Redux store and questionId, quesiton and answers in local storage
      */
     const handleRedirectQ = async () => {
+        console.log("handleRedirectQ")
         const retrievedAnswers = await answerRemote.getAnswersByQuestionId(props.question.id, 10, 0);
         props.clickQuestion(props.question);
         localStorage.setItem("questionId", JSON.stringify(props.question.id));
@@ -60,6 +71,7 @@ export const FeedBoxComponent: React.FC<FeedBoxComponentProps> = (props) => {
      */
 
     const handleRedirectA = async () => {
+        console.log("handleRedirectA")
         const retrievedQuestion = await questionRemote.getQuestionByQuestionId(props.question.questionId);
         const retrievedAnswers = await answerRemote.getAnswersByQuestionId(props.question.questionId, 10, 0);
         localStorage.setItem("questionId", JSON.stringify(retrievedQuestion.id));
@@ -69,43 +81,55 @@ export const FeedBoxComponent: React.FC<FeedBoxComponentProps> = (props) => {
         history.push('/forum');
     }
 
-    const addQuestionToFAQ = () =>{
-
+    /**
+     * Redirects you to a page to FAQ page TODO
+     */
+    const handleRedirectFAQ = async (e:React.SyntheticEvent) =>  {
+        e.stopPropagation()
+        setOpen(true)
         console.log("add questions")
+
     }
+    
+
 
     const questionContent = EditorState.createWithContent(convertFromRaw(JSON.parse(props.question.content)));
     const onChange = () => { };
 
     //!First box here contains answers not questions, so does its handler deal with answer not questions
     return (
+        <>
+        <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
+            <AddFAQComponent/>
+        </Backdrop>
         <Box display="flex" justifyContent="center" >
             <Card className={classes.boxInternal}>
                 {props.question.questionId ?
-                    <Box display="flex" justifyContent="center">
+                    <Box display="flex" justifyContent="center" onClick={() => handleRedirectA()}>
                         <Box paddingLeft={2} paddingRight={2} >
                             <div className={classes.divInternal}><Editor editorState={questionContent} readOnly={true} onChange={onChange} /></div>
                             <h3>{props.question.userId}</h3>
                             <p>{props.question.creationDate}</p>
-                            <AddCircleIcon onClick={() => handleRedirectA()} id="addQuestionFAQButton"/>
+                            <AddCircleIcon onClick={handleRedirectFAQ} id="addQuestionFAQButton"/>
 
                         </Box>
                     </Box>
                     :
                     <Box>
-                        <Box display="flex" justifyContent="center">
+                        <Box display="flex" justifyContent="center" onClick={() => handleRedirectQ()}>
                             <Box paddingLeft={2} paddingRight={2}>
                                 <h2>{props.question.title}</h2>
                                 <div><Editor editorState={questionContent} readOnly={true} onChange={onChange} /></div>
                                 <h3>{props.question.userId}</h3>
                                 <p>{props.question.creationDate}</p>
-                                 <AddCircleIcon onClick={() => handleRedirectQ()} id="addQuestionFAQButton"/>
+                                 <AddCircleIcon onClick={handleRedirectFAQ} id="addQuestionFAQButton"/>
 
                             </Box>
                         </Box>
                     </Box>}
             </Card>
         </Box>
+        </>
     )
 }
 
