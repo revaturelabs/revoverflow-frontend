bb25ef1c3871e23c726eb04101af448c8752282b
Colin Shaw <colin.shaw2@uoit.net>
2021-01-31 13:48:04 -0500

    changed FAQ model

--- src/models/faquestion.ts
+++ src/models/faquestion.ts
@@ -3,11 +3,13 @@
  * @author Ramninder Singh
  */
 
+import { Answer } from "./answer";
+import { Question } from "./question";
+
 
 
 export interface FAQuestion{
-
-    question : String;
-    answer : String;
-    location: String;
+    userId:number,
+    question: Question,
+    answer: Answer
 }
