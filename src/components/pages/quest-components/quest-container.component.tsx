a39c0c0652cc2a24b534a55a8080138e2278d0e3
Jinyeong Kim <jkim495@myseneca.ca>
2021-01-30 21:25:45 -0500

    mod question page

--- src/components/pages/quest-components/quest-container.component.tsx
+++ src/components/pages/quest-components/quest-container.component.tsx
@@ -1,7 +1,5 @@
 
 import React, { useState } from 'react';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
 import { Checkbox } from '@material-ui/core';
 import Pagination from '@material-ui/lab/Pagination';
 import QuestBoxComponent from './quest-box.component';
@@ -61,41 +59,32 @@ export interface QuestContainerComponentProps {
 export const  QuestContainerComponent: React.FC<QuestContainerComponentProps> = (props) => {
     const classes = useStyles();
     const [value, setValue] = React.useState(props.storeTab);
-    const [revatureBasedQuestion, setRevatureBasedQuestion] = useState(true);
-    const [locationBasedQuestion, setLocationBasedQuestion] = useState(true);
-    const [allQuestions, setAllQuestions] = useState(true);
+    const [revatureBasedQuestion, setRevatureBasedQuestion] = useState(false);
+    const [locationBasedQuestion, setLocationBasedQuestion] = useState(false);
     const userId = (+JSON.parse(JSON.stringify(localStorage.getItem('userId'))));
     const size = 10;
-    const forceUpdate = useForceUpdate();
 
     const toggleQuestion = (e:any) => {
-       // const forceUpdate = useForceUpdate();
-       // const forceUpdate = () => {
-                
-                if(e.target.name === 'revatureChk') setRevatureBasedQuestion(e.target.checked)
-                else setLocationBasedQuestion(e.target.checked)
-                forceUpdate();
-                load(0)
-                //WHY?!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! getting upset!!!!!!
-               
-
-    };
-
-    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-        setValue(newValue);
+        if(e.target.name === 'revatureChk'){
+            load(e.target.checked, locationBasedQuestion, 0)
+            setRevatureBasedQuestion(e.target.checked)
+        }else{
+            load(revatureBasedQuestion,e.target.checked, 0)
+            setLocationBasedQuestion(e.target.checked)
+        }
     };
 
     const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        load(value - 1);
+        load(revatureBasedQuestion,locationBasedQuestion, value - 1);
     };
 
-    const load = async (page: number) => {
+    const load = async (revatuure:boolean, location:boolean, page: number) => {
         let retrievedPageable: any;
         let tab: any;
-        console.log("revatureBasedQuestion: " + revatureBasedQuestion)
-        console.log("locationBasedQuestion: " + locationBasedQuestion)
-        if (revatureBasedQuestion === true) {
-            if(locationBasedQuestion === false){
+        console.log("revatuure: " + revatuure)
+        console.log("location: " + location)
+        if (revatuure) {
+            if(!location){
                 retrievedPageable = await questionRemote.getAllRevatureQuestions(size, page, true);
                 //tab = 0; 
                 if (retrievedPageable.numberOfElements === 0) {
@@ -103,13 +92,12 @@ export const  QuestContainerComponent: React.FC<QuestContainerComponentProps> = 
                  }
             }else{
                 retrievedPageable = await questionRemote.getAllQuestions(userId, page);
-                
                 if (retrievedPageable.numberOfElements === 0) {
                     return;
                 }
             }
         } else {
-            if(locationBasedQuestion === true){
+            if(location){
                 //SHOULD BE CHANGED!!!!
                 retrievedPageable = await questionRemote.getQuestionsByUserId(userId, size, page);
                 //tab = 1;
@@ -117,12 +105,11 @@ export const  QuestContainerComponent: React.FC<QuestContainerComponentProps> = 
                 return;
             }
         }
-
         props.clickTab(retrievedPageable.content, tab, retrievedPageable.totalPages, retrievedPageable.number);
     }
 
     if (props.storeQuestions.length === 0 ) {
-         load(0);
+         load(revatureBasedQuestion,locationBasedQuestion,0);
     }
 
     const renderQuestBoxComponents = () => {
@@ -138,25 +125,20 @@ export const  QuestContainerComponent: React.FC<QuestContainerComponentProps> = 
             <BreadcrumbBarComponent />
             <Container className={classes.containerInternal}>
                 <ThemeProvider theme={theme} >
-                    <Box justifyContent="center" display="flex" className={classes.boxExternal}>
-                        <Checkbox id="chk1" name="revatureChk" defaultChecked 
+                    <Box justifyContent="center" display="flex"  className={classes.boxExternal}>
+                        <Checkbox id="chk1" name="revatureChk"  
                                   icon={<BusinessIcon fontSize="large"/>}  className={classes.boxInternal} checked={revatureBasedQuestion} 
-                                    onChange={toggleQuestion} />
-                        <Checkbox id="chk2" name="locationChk" defaultChecked 
+                                  onChange={toggleQuestion} />
+                        <Checkbox id="chk2" name="locationChk"  
                                   icon={<LocationOnIcon fontSize="large" />} className={classes.boxInternal} checked={locationBasedQuestion} 
-                                   onChange={toggleQuestion} />
+                                  onChange={toggleQuestion} />
                         
                     </Box>
                     <div style={{ width: '100%' }}>
-                       {
-                           /* <Box display="flex" flexDirection="column" justifyContent="center" >
-                                {renderQuestBoxComponents()}
-                              </Box>*/
-                       } 
                         <Box display="flex" flexDirection="column" justifyContent="center" >
                             {/*renderQuestBoxComponents()*/}
                             { locationBasedQuestion ? <CustomizedBreadcrumbs />  : ''}
-                            { renderQuestBoxComponents()}
+                            { (locationBasedQuestion || revatureBasedQuestion) ? renderQuestBoxComponents() : ''}
                         </Box>
                     </div>
                     <Box display="flex" justifyContent="center" padding={5}>
