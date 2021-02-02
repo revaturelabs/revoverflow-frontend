0b7f84a8facbd9e4adf33f1eb936f02a09f21c21
Syed Azib <syed.azib@hotmail.com>
2021-02-02 15:35:20 -0500

    removed internal axios

--- src/remotes/login.remote.ts
+++ src/remotes/login.remote.ts
@@ -3,7 +3,7 @@
  * @author Michel Charles <mcharl05@nyit.edu>
  */
 
-import { authAxios, internalAxios } from './internal.axios';
+import { authAxios } from './internal.axios';
 
 export const getUserById = async (email: string | null) => {
     const response = await authAxios.get(`/user/${email}/points`);
